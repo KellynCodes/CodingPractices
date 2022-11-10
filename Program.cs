59465e6d128fb76659ba2480c4d27b47d5e9694a
prokelly <kellyrobot247@gmail.com>
2022-11-10 13:02:37 +0100

    Best programming coding practices

--- Program.cs
+++ Program.cs
@@ -1,10 +1,13 @@
 ﻿using BenzaoSessionOne.CodingPractices;
 
-namespace CodingPractices{
-    class Program{
-        public static void Main(string[] args){
-            User.GoodPractices();
-            User.BadPractice();
+namespace CodingPractices
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //User.GoodPractices();
+            User.BadPractice("Kelly", 44, "Nigeria", 'M');
         }
     }
 }
